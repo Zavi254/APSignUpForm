80c598eda8f57a08764632e66341bea2b485125e
Steven Otieno <steveotianga8991@gmail.com>
2023-04-04 15:38:33 +0300

    edited the verified route page

--- src/pages/Verified.jsx
+++ src/pages/Verified.jsx
@@ -1,23 +1,25 @@
-import Button from "../components/Button";
+import { Link } from "react-router-dom";
 import Image from "../components/Image";
 
 const Verified = () => {
   return (
-    <div className="verifiedContainer d-flex flex-column align-items-center
-    ">
-      <Image
-        imageLink={
-          "https://res.cloudinary.com/dogmqg8to/image/upload/v1678606109/AP%20Registration/tick_icon_bwppcs.png"
-        }
-        className={"img-fluid"}
-      />
-      <h3 className="fontColor mb-5">Your account is verified!</h3>
-      <p className="text-center">
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-        ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.
-      </p>
-      <Button text={'Login'} className={'btn signUpBtn fontColor'} />
+    <div
+      className="verifiedContainer d-flex flex-column justify-content-center align-items-center
+    "
+    >
+      <div className="innerVerifiedContainer d-flex flex-column align-items-center">
+        <Image
+          imageLink={
+            "https://res.cloudinary.com/dogmqg8to/image/upload/v1678606109/AP%20Registration/tick_icon_bwppcs.png"
+          }
+          className={"img-fluid"}
+        />
+        <h3>Your account is verified!</h3>
+        <p>We're excited to welcome you!</p>
+        <Link to={"#"} className={"btn signUpBtn"}>
+          Get Started
+        </Link>
+      </div>
     </div>
   );
 };
